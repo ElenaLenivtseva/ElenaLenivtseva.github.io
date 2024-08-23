d1e3ef4de863ce7242b6b376c69fa281d85bb3d4
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-08-23 18:18:42 +0700

    redux удален из зависимостей, также удалены слайсы, стор.

--- src/features/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import usersReducer from './slices/usersSlice'
-
-
-export default configureStore({
-	reducer: {
-		users: usersReducer,
-	},
-});
